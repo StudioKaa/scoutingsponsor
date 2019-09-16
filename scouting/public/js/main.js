cc4000add8e06a3d6861644dac7323bcd7f29422
Unknown <roks.freek@gmail.com>
2019-09-16 12:40:48 +0200

    fixed js

--- scouting/public/js/main.js
+++ scouting/public/js/main.js
@@ -4,10 +4,17 @@ function popUp() {
 
 function authorise() {
     //zet op de body -> onload="authorise()"
+    //geef je body de id body
 
+    let body = document.getElementById('body');
     let auth = prompt('fill in the password', '');
 
-    if (auth !== "password") {
+    body.style.display = "none";
+
+    if (auth !== "PaSsWoRd") {
         window.location = "{{ route('zet hir de route naar de pagina als je een verkeerd wachtwoord invult') }}";
+    }
+    else{
+        body.style.display = "block";
     }
 }
