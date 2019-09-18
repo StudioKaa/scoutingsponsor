99eb353a571993703d31822f6ae1b65a7c019021
Mohammed <hamadashw@outlook.com>
2019-09-18 15:43:22 +0200

    Hier de new css

--- scouting/resources/views/plots/show.blade.php
+++ scouting/resources/views/plots/show.blade.php
@@ -5,35 +5,41 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-</head>
+
+
+
 <?php
 
 
 ?>
-<body>
-    <h1>Plot naam: {{$plot->plotname}}</h1>
+<body  style="background: #66b266;">
+<div >
+
+
+    <h1 style="color: #CCE5CC; margin: 20px;" >Plot naam: {{$plot->plotname}}</h1>
     @if($plot->sold == 0)
-    
-        <p>Dit plot is nog niet verkocht!</p>
+
+        <p style="color: #CCE5CC; margin: 20px;">Dit plot is nog niet verkocht!</p>
 
     @elseif($plot->sold == 1)
-        <p>dit plot is bijna verkocht</p>
+        <p style="color: #CCE5CC; margin: 20px;">dit plot is bijna verkocht</p>
     @else
 
-        <p>Helaas!</p>
-        <p>Dit plot is al verkocht aan {{$sponsor->plotname}}.</p>
-    
-    @endif
-
-    @if($plot->sold == 0) 
-
-        <a href="{{ route('plots.edit', $plot->id) }}">Koop Plot</a><br><br>  
+        <p style="color: #CCE5CC; margin: 20px;">Helaas!</p>
+        <p style="color: #CCE5CC; margin: 20px;">Dit plot is al verkocht aan {{$sponsor->plotname}}.</p>
 
     @endif
-  
-    <a href="{{ route('home') }}">Go back to all plots</a>
+
+    @if($plot->sold == 0)
+
+        <a style="color: #CCE5CC; margin: 20px;" href="{{ route('plots.edit', $plot->id) }}">Koop Plot</a><br><br>
+
+    @endif
+
+    <a style="color: #CCE5CC; margin: 20px;"  href="{{ route('home') }}">Go back to all plots</a>
 
 
+</div>
 </div>
 
 </body>
